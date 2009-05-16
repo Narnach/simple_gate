969811b1adf95fef29f286f07baa4b71b190fa27
Wes Oldenbeuving <narnach@gmail.com>
2009-05-16 07:45:21 +0200

    Documented Router in yardoc style

--- lib/simple_gate/router.rb
+++ lib/simple_gate/router.rb
@@ -1,25 +1,33 @@
 class Router
-  # Graph of server connections. 
-  # It's a Hash of Arrays, which contains strings. Keys are server names with
-  # a connection to other servers. Each of these servers is a string in the
-  # associated Array.
-  # An example graph with three servers 'foo', 'bar' and 'baz'.
-  #
-  #   router = Router.new
-  #   router.paths = {
-  #     'foo' => %w[bar],
-  #     'bar' => ['baz']
-  #   }
-  #   router.find('foo', 'baz') #=> ['foo', 'bar', 'baz']
+  # Graph of server connections.
   attr_accessor :paths
 
+  # Create a new Router for a network of interconnected servers.
+  # An example connections graph with three servers 'foo', 'bar' and 'baz':
+  #
+  #   router = Router.new
+  #   # Define a graph: foo -> bar -> baz
+  #   router.paths = {
+  #     'foo' => %w[bar],
+  #     'bar' => %w[baz]
+  #   }
+  #   router.find('foo', 'baz') #=> ['foo', 'bar', 'baz']
+  # @param [Hash] paths Graph of server connections.
+  #   It's a Hash of Arrays, which contains strings. Keys are server
+  #   names with a connection to other servers. Each of these servers is a
+  #   string in the associated Array.
   def initialize(paths={})
     @paths = paths
   end
 
-  # A simple pathfinder. Returns a route as Array or nil.
+  # A simple recursive pathfinder.
   # Uses a very naieve depth-first recursive full-graph search to
   # find the shortest route.
+  # @param [String] start The node to start searching from for +target+.
+  # @param [String] target The node to look for. Once it is found, return the shortest route to it.
+  # @param [Array] current_route Internal variable that holds the route so far. Helps in preventing cyclic routes from being checked an infinite amount of times.
+  # @return [Array] The sequence of nodes that connect +start+ to +target+.
+  # @return [nil] When no route was found.
   def find(start, target, current_route = [])
     return [target] if start == target
     return nil unless paths.has_key?(start)
