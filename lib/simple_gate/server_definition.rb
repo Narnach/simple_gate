5fe5ccedffc32dcc2cbb1526e1682a035dfffe74
Wes Oldenbeuving <narnach@gmail.com>
2009-05-16 08:18:48 +0200

    Documented ServerDefinition

--- lib/simple_gate/server_definition.rb
+++ lib/simple_gate/server_definition.rb
@@ -2,6 +2,13 @@ require 'yaml'
 class ServerDefinition
   attr_accessor :host, :user, :options, :port, :password
   
+  # Create a new ServerDefinition.
+  #
+  # @param [String] host Name of server.
+  # @param [Hash] options Override the server's own configuration.
+  # @option options [Integer] port (22) Server port
+  # @option options [String] user SSH user
+  # @option options [String] password SSH password
   def initialize(host, options = {})
     @options = {:port => 22}.merge(options)
     self.host = host
@@ -25,20 +32,32 @@ class ServerDefinition
     @password=password
   end
 
+  # Yield connection information.
+  # @yieldparam [String] host SSH host
+  # @yieldparam [String] user SSH user
+  # @yieldparam [Hash] options SSH connection options
+  # @see ServerDefinition#initialize
+  # @see ServerDefinition#ssh_options
   def connection_info(&block)
     block.call(host, user, options.merge(ssh_options))
   end
-  
+
+  # Default SSH options
+  # @return [Hash]
   def ssh_options
     {
       :auth_methods => %w[password keyboard-interactive]
     }
   end
-  
+
+  # Represent server definition as URL-like string
+  # @return [String]
   def to_s
     "#{user}:#{'*' * password.to_s.size}@#{host}:#{port}"
   end
   
+  # Factory method that uses pre-defined server configurations.
+  # @return [ServerDefinition]
   def self.lookup(server)
     server = servers[server]
     new(server['address'],{ 
@@ -47,11 +66,19 @@ class ServerDefinition
       :port => server['port']
     })
   end
-  
+
+  # Factory method that chooses between a lookup and parse.
+  # @param [String] server Server name or ssh string
+  # @return [ServerDefinition]
+  # @see ServerDefinition.lookup
+  # @see ServerDefinition.parse
   def self.find(server)
     servers.has_key?(server) ? lookup(server) : parse(server)
   end
   
+  # Factory method that parses a connection string.
+  # @param [String] ssh_string String formatted as "user:password@host:port"
+  # @return [ServerDefinition]
   def self.parse(ssh_string)
     user, password, host, port = ssh_string.match /\A(.*?):(.*?)@(.*?):(\d*?)\Z/
     new(host, :user => user, :password => password, :port => port)
@@ -76,6 +103,7 @@ class ServerDefinition
     #
     # Since the parsed Hash of servers is cached, a value can be stored and
     # the configuration file ignored if desired.
+    # @return [Hash] Server name to server configuration pairs.
     def servers
       @servers ||= YAML.load_file(File.expand_path('~/.servers.yml'))
     end
